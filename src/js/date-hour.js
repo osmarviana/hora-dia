54bd52bd26b48dcbf18490b55a51fa927730d6e0
Osmar Viana <osmarvianatorres@gmail.com>
2023-05-08 18:10:41 -0300

    "Atualizando imagens"

--- src/js/date-hour.js
+++ src/js/date-hour.js
@@ -98,16 +98,30 @@ function clock() {
 
   hourNow.innerText = timeNow;
 
-  if (hour > 5 && hour < 18) {
+  if (hour >= 5 && hour < 12) {
     document.body.style.backgroundImage =
       "linear-gradient(rgba(186, 230, 231, 1), rgba(61, 106, 116, 0.2))";
     container.style.backgroundColor = "rgb(0, 89, 255)";
-    imgContainer.innerHTML = `<img src="../src/img/dia.jpg">`;
+    imgContainer.innerHTML = `
+    <img src="https://cdn-icons-png.flaticon.com/512/3892/3892928.png" alt"Imagem dia">
+    <h1 class="dia">Bom dia!</h1>
+    `;
+  } else if (hour >= 12 && hour < 18) {
+    document.body.style.backgroundImage =
+      "linear-gradient(rgb(186, 200, 231), rgba(61, 76, 116, 0.2))";
+    container.style.backgroundColor = "rgb(55, 0, 255)";
+    imgContainer.innerHTML = `
+    <img src="https://cdn-icons-png.flaticon.com/512/3892/3892928.png" alt"Imagem dia">
+    <h1 class="tarde">Boa tarde!</h1>
+    `;
   } else {
     document.body.style.backgroundImage =
       "linear-gradient(rgba(44, 21, 59, 1), rgba(15, 3, 36, 0.8)";
     container.style.backgroundColor = "rgb(17, 6, 43)";
-    imgContainer.innerHTML = `<img src="../src/img/noite.jpg">`;
+    imgContainer.innerHTML = `
+    <img src="https://cdn-icons-png.flaticon.com/512/1809/1809597.png" alt"Imagem noite">
+    <h1 class="noite">Boa noite!</h1>
+    `;
   }
 }
 
